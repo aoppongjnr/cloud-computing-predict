003a9cecc1e6becfee12f3f598c21dfe237f4742
aoppongjnr <aoppongjnr@gmail.com>
2022-10-24 10:41:05 +0000

    updated contact.js with API Gateway URL

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "{https://54amxz1jj9.execute-api.us-east-2.amazonaws.com/default}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
